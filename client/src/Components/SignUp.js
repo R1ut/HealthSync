2a2819f27727d69286ece8b3179ee03ecfc52348
R1ut <rgoya1@ocdsb.ca>
2025-07-06 19:40:35 -0400

    I implemented a pop up screen after sign up for user to agree with their medical info being stored in the website. At first I tried to create a different permission script and import that function to signup.js and make it start using onclick. However, since i had multiple onclicks i didn't work. THere I changed so that I made a variable of permission using true flase statments and pendingNav. Then i made 2 functions handlesumbitclick and handlepermissionresponse. One's purpose to work when button click and make both variables true and the seconds checks if the permission response is a yes or no and based on that navigate to interview page. I then made a container for perimission when button clicked and had the the background overlay, title, words and yes or no button styled in css.

--- client/src/Components/SignUp.js
+++ client/src/Components/SignUp.js
@@ -1,55 +1,72 @@
-import './SignUp.css'
-import React from 'react'
-import email_icon from './Assets/email.png'
-import password_icon from './Assets/password.png'
-import name_icon from './Assets/Name.png'
+import './SignUp.css';
+import React, { useState } from 'react';
+import email_icon from './Assets/email.png';
+import password_icon from './Assets/password.png';
+import name_icon from './Assets/Name.png';
 import { useNavigate } from "react-router-dom";
 
-export default function permission()
-{
-  reuturn(
-    <div className="PerBox"
-  )
-
-}
-export default function SignUp() 
-
-{
+export default function SignUp() {
+  const [permission, setPermission] = useState(false);
+  const [pendingNavigation, setPendingNavigation] = useState(false);
   const navigate = useNavigate();
 
+  function handleSubmitClick() {
+    setPermission(true); 
+    setPendingNavigation(true); //  Set flag to allow nav after permission
+  }
+
+  function handlePermissionResponse(response) {
+    if (response === 'yes' && pendingNavigation) {
+      navigate("/interview");
+    }
+    setPermission(false);
+    setPendingNavigation(false);
+  }
+
   return (
-  
-    <div className = 'container'>
+    <div className="container">
       <div className="header">
-        <div className = "text">Sign Up</div>
-        <div className ="underline"></div>
+        <div className="text">Sign Up</div>
+        <div className="underline"></div>
       </div>
+
       <div className="inputs">
-        <div className = "input">
-          <img src= {name_icon} alt="" style={{ width: "30px", height: "25px", marginRight: "0.5rem" }} />
-          <input type="type" placeholder="Name"/>
+        <div className="input">
+          <img src={name_icon} alt="" style={{ width: "30px", height: "25px", marginRight: "0.5rem" }} />
+          <input type="text" placeholder="Name" />
         </div>
-        <div className = "input">
-          <img src= {email_icon} alt="" style={{ width: "20px", height: "15px", marginRight: "0.5rem" }} />
-          <input type="email" placeholder="Email ID"/>
+        <div className="input">
+          <img src={email_icon} alt="" style={{ width: "20px", height: "15px", marginRight: "0.5rem" }} />
+          <input type="email" placeholder="Email ID" />
         </div>
-        <div className = "input">
-          <img src= {password_icon} alt="" style={{ width: "20px", height: "22px", marginRight: "0.5rem" }} />
-          <input type="password" placeholder="Password"/>
+        <div className="input">
+          <img src={password_icon} alt="" style={{ width: "20px", height: "22px", marginRight: "0.5rem" }} />
+          <input type="password" placeholder="Password" />
         </div>
       </div>
-      <div className = "submit-container">
-       <div
-          className="submit"
-          onClick={() => navigate("/interview")}
-          style={{ cursor: "pointer" }}
-        >
+
+      <div className="submit-container">
+        <div className="submit" onClick={handleSubmitClick} style={{ cursor: "pointer" }}>
           Sign Up
         </div>
       </div>
+
+      {permission && (
+        <div className='overlay'>
+          <div className="PerBox">
+            <div className='together'>
+              <h2>Permission</h2>
+              <button onClick={()=> handlePermissionResponse('yes')}>X</button>
+            </div>
+            <p>
+              Do you give permission to have your data and medical reports entered by you to stay on this website? Your data will be kept private and will not be shared with anyone. Do you agree?
+            </p>
+            <button className="Yes" onClick={() => handlePermissionResponse('yes')}>Yes</button>
+            <button className="No" onClick={() => handlePermissionResponse('no')}>No</button>
+          </div>
+        </div>
+      )}
     </div>
-
-  )
-  
-
+  );
 }
+//Make that when hover on the x create a tag that shows that do it for later.
